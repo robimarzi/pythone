46760d61022c4906bb023fcc1af357fb6c7de6c2
robi <robertomarzi@live.com>
2025-01-27 10:45:53 +0100

    Update media.py

--- media.py
+++ media.py
@@ -1,15 +1,15 @@
 #programma per calcolare la media
-count = int(0)   #var. contatirce per la media
-numero = "0"     #input utente 
+contatore = int(0)   #var. contatirce per la media
+numero = "0"      #input utente 
 somma = int(0)   #somma degli in put
-dato = input("Inserisci i numeri da sommare per fare la media, scrivi altro per avere la media: ")
+dato = input("Inserisci i numeri da sommare per fare la media, scrivi altro per avere il risultato: ")
 while dato.isdigit() :
     
     numero = int(dato)
     somma  = int(numero + somma)
     dato   = input()
-    count  = count + 1
+    contatore  += 1
 
-print("La media dei numeri scritti è", somma/count )
+print("La media dei numeri scritti è", somma/contatore)
 
 
